d0144c17a5432e6d2cf92e6869e06ea73529f914
DmytroTarasiuk <dmytro.tarasiuk1899@gmail.com>
2023-12-01 20:35:48 +0100

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import Dashboard from "./components/Dashboard";
+import Layout from "./components/Layout";
+
+interface ILayoutRoute {
+  exact?: boolean;
+  path: string;
+  component: ReactNode;
+}
 
 function App() {
+  const LayoutRoute = ({ exact, path, component }: ILayoutRoute) => {
+    return (
+      <Route
+        exact={exact}
+        path={path}
+        render={() => {
+          return <Layout>{component}</Layout>;
+        }}
+      ></Route>
+    );
+  };
+
   return (
-    <>
-      <p>Project starter</p>
-    </>
+    <Router>
+      <Switch>
+        <LayoutRoute exact path="/dashboard" component={<Dashboard />} />
+        <Redirect from="/" to="/dashboard" />
+      </Switch>
+    </Router>
   );
 }
 
