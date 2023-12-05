c34f5b286a1c19c0bee7dc37eac52a7fe47c622f
DmytroTarasiuk <dmytro.tarasiuk1899@gmail.com>
2023-12-05 13:15:38 +0100

    add clear orders action

--- src/redux/orders/reducers.ts
+++ src/redux/orders/reducers.ts
@@ -1,6 +1,6 @@
 import AppAction from "../action";
 
-import { SET_ORDER_LIST } from "./actions";
+import { CLEAR_ORDER_LIST, SET_ORDER_LIST } from "./actions";
 
 const initialState = {
   orderss: [],
@@ -10,6 +10,8 @@ export function orderReducer(state = initialState, action: AppAction) {
   switch (action.type) {
     case SET_ORDER_LIST:
       return { ...state, orders: action.payload };
+    case CLEAR_ORDER_LIST:
+      return { ...state, orders: [] };
 
     default:
       return state;
